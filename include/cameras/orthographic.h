f338ee93c46ce0147ecc9c66d3fadcc6f95831e0
Alex Levy <ajlevy246@gmail.com>
2025-11-06 21:21:52 -0500

    Added orthographic projection camera implementation

--- include/cameras/orthographic.h
+++ include/cameras/orthographic.h
@@ -1,3 +1,17 @@
 #pragma once
 
 #include "camera.h"
+
+class OrthographicCam : public Camera {
+public:
+    // ===== MEMBERS =====
+    Vec3 eye, u, v, w;
+    double aspect, fov;
+        
+    // ===== CONSTRUCTORS =====
+    OrthographicCam();
+    OrthographicCam(Vec3 look_from, Vec3 look_at, double aspect);
+
+    // ===== METHODS =====
+    Ray generate_ray(int x, int y, int width, int height);
+};
---
+++ src/cameras/orthographic.cpp
@@ -0,0 +1,33 @@
+#include "raytracing.h"
+#include "cameras.h"
+
+
+// ===== CONSTRUCTORS =====
+OrthographicCam::OrthographicCam() {
+    aspect = 1.0;
+    
+    eye = Vec3(1, 1, 1);
+    Vec3 look_at = Vec3(-1, -1, -1);
+    Vec3 up = Vec3(0, 0, 1);
+    w = eye - look_at;
+    u = up.cross(w).normalized();
+    v = w.cross(u).normalized();
+};
+
+OrthographicCam::OrthographicCam(Vec3 look_from, Vec3 look_at, double aspect_) {
+    aspect = aspect_;
+
+    Vec3 up = Vec3(0, 0, 1);
+    eye = look_from;
+    w = eye - look_at;
+    u = up.cross(w).normalized();
+    v = w.cross(u).normalized();
+}
+
+Ray OrthographicCam::generate_ray(int x, int y, int width, int height) {
+    double xn = (2.0 * x / width - 1.0) * aspect;
+    double yn = (1.0 - 2.0 * y / height);
+    Vec3 direction = (w * -1).normalized();
+    Vec3 origin = eye + u * xn + v * yn;
+    return Ray(origin, direction);
+}
